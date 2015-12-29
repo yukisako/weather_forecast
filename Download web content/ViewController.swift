798c78ac205bbb75b4e4be73d5dde6a8eecf2311
yukisako <yukisakoatoz@gmail.com>
2015-12-30 00:22:16 +0900

    finish making app

--- Download web content/ViewController.swift
+++ Download web content/ViewController.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.github.com")!
+        
+        webView.loadRequest(NSURLRequest(URL: url))
+        
+        
+        
+        
+        /*
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            //タスクが終了した時に実行されるメソッド．クロージャとか呼ばれる．
+            if let urlContent = data{
+            //オプショナルバインディングをしている
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.webView.loadHTMLString(String(webContent), baseURL: nil)
+                })
+                
+            } else {
+                //データがない時に実行
+                
+                
+            }
+        }
+        
+        task.resume()
+        */
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
